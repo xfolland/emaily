85879913f61649a1a22eb315f3f166c82b300d72
Xavier Folland <xfolland@protonmail.com>
2021-09-21 11:06:10 +0800

    testing config vars

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const express = require("express");
+const passport = require("passport");
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("response");
+  res.send(process.env.TEST);
 });
+// passport.use(
+//   new GoogleStrategy({
+//     clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+//   })
+// );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
